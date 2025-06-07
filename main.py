227ecfbbec29850db83e018040ffaa95747f32fc
tombomann <tomarnejensen@gmail.com>
2025-06-07 05:30:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Gjør public/ tilgjengelig for nettleseren på rot
 app.mount("/", StaticFiles(directory="public", html=True), name="static")
-
