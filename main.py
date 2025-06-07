7a797559375c0613fab39093cc1396fbc13b81af
tombomann <tomarnejensen@gmail.com>
2025-06-07 16:34:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Gjør public-mappen synlig (f.eks. test.html og embed.js)
 app.mount("/", StaticFiles(directory="public", html=True), name="static")
