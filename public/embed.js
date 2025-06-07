33a625d81a6f2e9528c58a4da070e91edecf410e
tombomann <tomarnejensen@gmail.com>
2025-06-07 16:33:25 +0200

    Update embed.js

--- public/embed.js
+++ public/embed.js
@@ -1,7 +1,6 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js";
 
-app = FastAPI()
-
-# Gjør public/ tilgjengelig som statiske filer
-app.mount("/", StaticFiles(directory="public", html=True), name="static")
+Chatbot.init({
+  chatflowid: "7ef90146-02e6-4708-b1ef-dc6c460fcb91",
+  apiHost: "https://elbatt-chatbot.onrender.com"
+});
