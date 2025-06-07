a7e38451ab8804235286ab3e9150305a70d3b01c
tombomann <tomarnejensen@gmail.com>
2025-06-07 14:52:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+# Gjør hele public/ mappen tilgjengelig
 app.mount("/", StaticFiles(directory="public", html=True), name="static")
 
+# Ekstra sikkerhet for embed.js
 @app.get("/embed.js")
 async def get_embed():
     return FileResponse("public/embed.js", media_type="application/javascript")
