3219d27f30bd615091bcecd79b0c0f5ebc999837
tombomann <tomarnejensen@gmail.com>
2025-06-07 05:42:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="public", html=True), name="static")
+# Eksponer public-mappe korrekt
+app.mount("/public", StaticFiles(directory="public"), name="public")
+
+# Spesifikk rute for embed.js
+@app.get("/embed.js")
+async def get_embed():
+    return FileResponse("public/embed.js", media_type="application/javascript")
