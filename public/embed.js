f41d1571ef96e63de27989fb75b65916bf449aea
tombomann <tomarnejensen@gmail.com>
2025-06-07 15:53:30 +0200

    Update embed.js

--- public/embed.js
+++ public/embed.js
@@ -2,10 +2,5 @@ import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
 
 Chatbot.init({
   chatflowid: "7ef90146-02e6-4708-b1ef-dc6c460fcb91",
-  apiHost: "https://elbatt-chatbot.onrender.com",
-  chatflowConfig: {
-    headers: {
-      "Authorization": "Bearer J0hCDNQSe3_lPbCHCj8TAClINVtYU1w9Ifs7ZvMCZvw"
-    }
-  }
+  apiHost: "https://elbatt-chatbot.onrender.com"
 })
