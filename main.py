b0a128e29c72d66aefc8f4700b815299658dcc26
tombomann <tomarnejensen@gmail.com>
2025-06-07 14:40:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-import os
 
 app = FastAPI()
 
-# Sørg for at hele public/ eksponeres korrekt
-app.mount("/public", StaticFiles(directory="public"), name="public")
+app.mount("/", StaticFiles(directory="public", html=True), name="static")
 
-# Eksakt rute for embed.js
 @app.get("/embed.js")
 async def get_embed():
-    file_path = os.path.join("public", "embed.js")
-    return FileResponse(file_path, media_type="application/javascript")
-
-# (valgfritt) testside
-@app.get("/test-chatbot.html")
-async def get_test():
-    return FileResponse("public/test-chatbot.html", media_type="text/html")
+    return FileResponse("public/embed.js", media_type="application/javascript")
